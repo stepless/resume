4f3c9159a3c9a15f4910658252cf36fd05d0af65
luma <madezhong1220@gmail.com>
2022-06-21 13:48:25 +0800

    添加乔巴

--- src/main.js
+++ src/main.js
@@ -3,37 +3,29 @@ var myChart = echarts.init(document.getElementById('skills'));
 
 // 指定图表的配置项和数据
 var option = {
-  tooltip: {},
-  radar: {
-      // shape: 'circle',
-      name: {
-          textStyle: {
-              color: '#fff',
-              backgroundColor: '#999',
-              borderRadius: 3,
-              padding: [3, 5]
-          }
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: 'Vue', max: 100 },
+          { name: '静态页面', max: 100 },
+          { name: '沟通能力', max: 100},
+          { name: '编程基础', max: 100 },
+          { name: '项目开发', max: 100 },
+          { name: 'JavaScript', max: 100 }
+        ]
       },
-      indicator: [
-        { name: '静态页面', max: 100},
-        { name: '编程基础', max: 100},
-        { name: 'Vue', max: 100},
-        { name: 'React', max: 100},
-        { name: '项目开发', max: 100},
-        { name: '沟通能力', max: 100}
+      series: [
+        {
+          name: 'spending',
+          type: 'radar',
+          data: [
+            {
+              value: [76, 90, 80, 62, 70, 82],
+              name: 'Allocated Budget'
+            }
+          ]
+        }
       ]
-  },
-  series: [{
-      name: '预算 vs 开销（Budget vs spending）',
-      type: 'radar',
-      // areaStyle: {normal: {}},
-      data: [
-          {
-              value: [100, 70, 90, 80, 85, 70],
-              name: '能力雷达'
-          }
-      ]
-  }]
 };
 
 // 使用刚指定的配置项和数据显示图表。
